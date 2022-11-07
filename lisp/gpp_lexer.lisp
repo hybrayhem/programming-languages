099c041a367a146d02d398a799d58e0fd5ec11f5
Halil I. ILHAN <hybrayhem@gmail.com>
2022-11-07 22:48:30 +0300

    2: lisp: get basic input

--- lisp/gpp_lexer.lisp
+++ lisp/gpp_lexer.lisp
@@ -1,36 +1,34 @@
-; gppinterpreter(zero or one input(input may be file name to interpret)) 
-; (format t "Hello, world!")
-; (setf keywords '(and or equal less nil list append concat set deffun for if exit load disp true false))
-; (print keywords)
+(setf operators '(#\+ #\- #\/ #\( #\) #\* #\" ))
+(setf keywords '(and or equal less nil list append concat set deffun for if exit load disp true false))
 
-; (defun lexer (input)
-; (
+(defun lexer (input)
+    (format t input)
+)
+    
+(defun readFile(filename)
+    ; get file name and print it
+    (format t filename)
+)
 
-; ))
-
-; (defun readFile(filename)
-; (
-;     ; get file name and print it
-;     ; (setf fileName (read-line))
-;     ; (format t "~s : " filename)
-; ))
-
-; (defun readTerminal()
-; (
-
-; ))
+(defun readTerminal()
+    ; get terminal command and print it
+    (loop
+        (terpri)
+        (format t "> ")
+        (setf command (read-line))
+        (lexer command)
+    )
+)
 
 
-(defun gppinterpreter (&optional gppfile) (
+(defun gppinterpreter (&optional gppfile)
+    (format t "G++ 1.8.2 Interpreter (default)")
+    (terpri)
+
     (if (eql gppfile nil) 
         (readTerminal) 
-        (readFile)
+        (readFile gppfile)
     )
+)
 
-    ; (format t "G++ 1.8.2 Interpreter (default)")
-    ; (loop
-    ;     (format t "> ")
-    ; )
-))
-
-(gppinterpreter (first *args*))
+(gppinterpreter (car *args*))
