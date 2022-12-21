8ee5405763664b02b08cd332d73e19605ca4c926
Halil I. ILHAN <hybrayhem@gmail.com>
2022-12-21 21:06:26 +0300

    2: add operand id getter

--- lisp/gpp_lexer.lisp
+++ lisp/gpp_lexer.lisp
@@ -1,8 +1,37 @@
-(setf operators '(#\+ #\- #\/ #\( #\) #\* #\" ))
-(setf keywords '(and or equal less nil list append concat set deffun for if exit load disp true false))
-
 (defun lexer (input)
-    (format t input)
+    ; (format t input)
+    
+    (let (tokens (list))
+        (loop for c across input
+            
+            do (
+                format t "~%~a" c
+                ; (cond 
+                ;         ((char= c #\+) (push (list :plus c) tokens))
+                ;         ((char= char #\-) (push (list :minus char) tokens))
+                ;         ((char= char #\/) (push (list :slash char) tokens))
+                ;         ((char= char #\*) (push (list :star char) tokens))
+                ;         (char= char #\()  (push (list :lparen char) tokens))
+                ;         ((char= char #\)) (push (list :rparen char) tokens))
+                ;         ((char= char #\") (push (list :quote char) tokens))
+                ;         ((char= char #\,) (push (list :comma char) tokens))
+
+                ;         ; ((char= char #\ ) (push (list :space char) tokens))
+                ; )
+                
+            )
+            
+        )
+    )
+    
+    
+    (format t tokens)
+)
+
+(defun getOperandId (op)
+    (if (member op (list :plus :minus :slash :star :lparen :rparen :quote :comma))
+        (format t "OP_~a" op)
+    )
 )
     
 (defun readFile(filename)
@@ -24,6 +53,8 @@
 (defun gppinterpreter (&optional gppfile)
     (format t "G++ 1.8.2 Interpreter (default)")
     (terpri)
+
+    (getOperandId :plus)
 
     (if (eql gppfile nil) 
         (readTerminal) 
