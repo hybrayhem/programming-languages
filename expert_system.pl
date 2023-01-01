8bc729223c2e47a9479c7e014af40a9cc89d3654
Halil I. ILHAN <hybrayhem@gmail.com>
2023-01-01 20:34:05 +0300

    4: design expert system

--- expert_system.pl
+++ expert_system.pl
@@ -1,2 +1,19 @@
+%%%% Experts System: room{}, course{}, student{}, instructor{}.
+
+% Room: id, capacity, opening_time, closing_time, occupants[], properties[projector, 
+% smart_board, disabled-accessibility, ...]
+%       occupant: start_time, end_time, course_name
+
+% Course: id, instructor, capacity, occupances[], requirements[projector, ...] 
+%       occupance: start_time, end_time, room
+
+% identity: id, name, disability
+% Instructor: identity, courses, requirements[projector, ...]
+% Student: identity, courses
+
+%%% Queries: check_room_conflicts, get_assigned_room, get_possible_room_class_pairs, 
+% can_student_enroll, get_possible_enrollments, add_student, add_course, add_room
+
+
 main :- 
     write('Expert System'), nl.
